5f0421f8edef0615dbf2281bebeb092b6c3cebf6
mskian <veerfun23@gmail.com>
2021-03-23 14:24:36 +0530

    ☁  Deploy

--- config.php
+++ config.php
@@ -1,3 +1,3 @@
 <?php
 
-$LIVE_URL = "https://m.cricbuzz.com/live-cricket-scores/32282/5th-t20i-england-tour-of-india-2021";
+$LIVE_URL = "https://m.cricbuzz.com/live-cricket-scores/32287/1st-odi-england-tour-of-india-2021";
