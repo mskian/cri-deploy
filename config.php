3e87becc271bed2956c18c21bd03ff6f57df5907
mskian <veerfun23@gmail.com>
2021-04-23 19:43:58 +0530

    ☁  Deploy

--- config.php
+++ config.php
@@ -1,3 +1,3 @@
 <?php
 
-$LIVE_URL = "https://m.cricbuzz.com/live-cricket-scores/35663/16th-match-indian-premier-league-2021";
+$LIVE_URL = "https://m.cricbuzz.com/live-cricket-scores/35668/17th-match-indian-premier-league-2021";
