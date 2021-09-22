d4a0d41a73f51ce67cf3254d239daf5a3dabcdfb
mskian <veerfun23@gmail.com>
2021-09-22 18:08:01 +0530

    ☁  Deploy

--- config.php
+++ config.php
@@ -1,3 +1,3 @@
 <?php
 
-$LIVE_URL = "https://m.cricbuzz.com/live-cricket-scores/37562/32nd-match-indian-premier-league-2021";
+$LIVE_URL = "https://m.cricbuzz.com/live-cricket-scores/37567/33rd-match-indian-premier-league-2021";
