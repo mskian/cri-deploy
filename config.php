8487fc7248813b26715e723577286c5de07bf753
mskian <veerfun23@gmail.com>
2021-11-14 17:57:33 +0530

    ☁  Deploy

--- config.php
+++ config.php
@@ -1,3 +1,3 @@
 <?php
 
-$LIVE_URL = "https://m.cricbuzz.com/live-cricket-scores/38091/28th-match-super-12-group-2-icc-mens-t20-world-cup-2021";
+$LIVE_URL = "https://m.cricbuzz.com/live-cricket-scores/38157/final-icc-mens-t20-world-cup-2021";
