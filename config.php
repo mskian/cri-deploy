9e93e5609da9e916f5cce3db7e06574684939d5e
mskian <veerfun23@gmail.com>
2021-11-26 10:33:08 +0530

    ☁  Deploy

--- config.php
+++ config.php
@@ -1,3 +1,3 @@
 <?php
 
-$LIVE_URL = "https://m.cricbuzz.com/live-cricket-scores/38157/final-icc-mens-t20-world-cup-2021";
+$LIVE_URL = "https://m.cricbuzz.com/live-cricket-scores/38582/1st-test-new-zealand-tour-of-india-2021";
