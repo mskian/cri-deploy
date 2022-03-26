cc11d34b8bb6f843eb355868e41bd4f4eb2bdd7f
mskian <veerfun23@gmail.com>
2022-03-26 11:56:06 +0530

    🌧  Deploy

--- config.php
+++ config.php
@@ -1,3 +1,3 @@
 <?php
 
-$LIVE_URL = "https://m.cricbuzz.com/live-cricket-scores/38582/1st-test-new-zealand-tour-of-india-2021";
+$LIVE_URL = "https://m.cricbuzz.com/live-cricket-scores/45886/1st-match-indian-premier-league-2022";
